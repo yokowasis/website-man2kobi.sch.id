1d44c9232a2c9dc321e12e70ee9d7520832afa1d
Wasis Sasoko - AIR <yokowasis@gmail.com>
2023-09-09 12:08:49 +0800

    optional image url

--- newpost.sh
+++ newpost.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Check if both parameters are provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <title> <content>"
+# Check if at least two parameters are provided
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <title> <content> [img_url]"
     exit 1
 fi
 
 # Read the title and content from command line parameters
 title="$1"
 content="$2"
+
+# Set the image URL if provided as the third parameter; otherwise, use a default URL
+img_url="${3:-https://bengkulu.kemenag.go.id/file/file/Gambar/logo2.jpg}"
 
 # Calculate yesterday's date in the format YYYY-MM-DD
 yesterday=$(date -u -v-1d +%Y-%m-%d)
@@ -35,7 +38,7 @@ echo "layout: post" >> "_posts/$filename"
 echo "title: \"$title\"" >> "_posts/$filename"
 echo "date: ${yesterday}" >> "_posts/$filename"
 echo "categories: Berita" >> "_posts/$filename"
-echo "img: https://bengkulu.kemenag.go.id/file/file/Gambar/logo2.jpg" >> "_posts/$filename"
+echo "img: $img_url" >> "_posts/$filename"
 echo "author: Tim Humas MAN 2 Kota Bima" >> "_posts/$filename"
 echo "editor: Tim Humas MAN 2 Kota Bima" >> "_posts/$filename"
 echo "---" >> "_posts/$filename"
